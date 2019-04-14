55889802390918613ca9e592079a959e6e819c64
tiptic <aa565530560@gmail.com>
2019-04-14 15:43:37 +0900

    Dev01 AI pathfinding function realized in BP.

---
+++ Source/p04_shootingGameNew/ChooseNextWaypoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P04_SHOOTINGGAMENEW_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
