3c4bd9438bc0059b161a078fedab986f1a05b4f0
tiptic <aa565530560@gmail.com>
2019-04-18 21:35:00 +0900

    Dev03  Befor refactoring FIRE function. Realized AI patrolling function and chasing enemies function, and aborting pathfinding task function.

--- Source/p04_shootingGameNew/p04_shootingGameNewGameMode.cpp
+++ Source/p04_shootingGameNew/p04_shootingGameNewGameMode.cpp
@@ -2,14 +2,14 @@
 
 #include "p04_shootingGameNewGameMode.h"
 #include "p04_shootingGameNewHUD.h"
-#include "p04_shootingGameNewCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 Ap04_shootingGameNewGameMode::Ap04_shootingGameNewGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
